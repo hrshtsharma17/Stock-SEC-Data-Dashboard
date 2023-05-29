25451eee52f22c099da240a6d1f38cc2a93717a4
hrshtsharma17 <hrshtsharma2012@gmail.com>
2023-05-29 15:39:44 -0500

    updated dbt sql

--- dbt/sec_transformed.sql
+++ dbt/sec_transformed.sql
@@ -1,18 +1,15 @@
-SELECT
-    tickers,
-    category,
-    sicdescription,
-    entitytype,
-    exchanges,
-    filingcount,
-    cik,
-    name,
-    stateofincorporation,
-    sic,
-    latestrevenuefilingdate,
-    latestrevenueq10value,
-    latestassestsfilingdate,
-    latestassestsq10value,
-    created_utc
-FROM
-    dev.public.sec_table
+SELECT tickers,
+        category,
+        entitytype,
+        exchanges,
+        filingcount,
+        cik,
+        name,
+        stateofincorporation,
+        latestrevenuefilingdate,
+        latestrevenueq10value,
+        latestassestsfilingdate,
+        latestassestsq10value,
+        created_utc::date as utc_date,
+        created_utc::time as utc_time
+FROM dev.public.sec_table
